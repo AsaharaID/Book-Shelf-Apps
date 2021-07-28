277e88ca5e956e742b877f59b166f6db9e15c17b
AsaharaID <muhamadsyahr45@gmail.com>
2021-07-28 23:36:09 +0700

    git remote add origin https://github.com/AsaharaID/Book-Shelf-Apps.git git branch -M main git push -u origin main

---
+++ BookShelfApps/js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded',function(){
+   const submitform = document.getElementById("form");
+   submitform.addEventListener("submit", function(event){
+       event.preventDefault();
+       addTodo();
+   });
+   
+   if(isStorageExist()){
+       loadDataFromStorage()
+   }
+});
+
+document.addEventListener("ondatasaved", () =>{
+    console.log("Data dah disimpen NGAB")
+})
+
+document.addEventListener("ondataloaded", () =>{
+    refreshDataFromTodos()
+})
